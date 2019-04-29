46723d90d0902cde1fc9baa91247cc907b332bd1
sathis <sathis62@gmail.com>
2019-04-30 03:10:38 +0530

    Remove unused import

--- src/main/java/com/intercom/geocircle/GeocircleApplication.java
+++ src/main/java/com/intercom/geocircle/GeocircleApplication.java
@@ -3,7 +3,6 @@ package com.intercom.geocircle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableCaching
